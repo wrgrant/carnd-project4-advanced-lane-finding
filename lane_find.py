62853513f972c896f7c1fb4a8e89f68c8f5b5899
William Grant <williamrgrant@gmail.com>
2017-04-03 18:38:46 -0400

    Add function to calculate line curvature. Still needs work.

--- lane_find.py
+++ lane_find.py
@@ -206,3 +206,24 @@ def plot_line_find_update():
     plt.show(block=False)
     plt.pause(.00001)
     plt.close()
+
+
+
+
+def calculate_line_curvature():
+    ploty = np.linspace(0, img.shape[0] - 1, img.shape[0])
+
+    # Define conversions in x and y from pixels space to meters
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/700 # meters per pixel in x dimension
+
+    # Fit new polynomials to x,y in world space
+    left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+
+    # Calculate the new radii of curvature
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+    # Now our radius of curvature is in meters
+    print(left_curverad, 'm', right_curverad, 'm')
+    # Example values: 632.1 m    626.2 m
