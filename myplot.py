c71aaa39e3fc5d503beac34fd9ed56f953af52b4
William Grant <williamrgrant@gmail.com>
2017-04-02 21:21:26 -0400

    Implemented threshold detection.

--- myplot.py
+++ myplot.py
@@ -1,10 +1,38 @@
 import matplotlib.pyplot as plt
 
 
+
+def close_event():
+    plt.close()
+
+
+def timed_plot_double(img1, img2, title1='', title2=''):
+    # Setup a timer
+    # fig = plt.figure()
+    # timer = fig.canvas.new_timer(interval=500)
+    # timer.add_callback(close_event)
+
+    # Do the plotting
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    f.tight_layout()
+    ax1.imshow(img1, cmap='gray')
+    ax1.set_title(title1, fontsize=20)
+    ax2.imshow(img2, cmap='gray')
+    ax2.set_title(title2, fontsize=20)
+
+    # Show with timer
+    # timer.start()
+    #plt.draw()
+    plt.show(block=False)
+    plt.pause(.00001)
+    plt.close()
+
+
+
 def plot_double(img1, img2, title1='', title2=''):
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
     f.tight_layout()
-    ax1.imshow(img1)
+    ax1.imshow(img1, cmap='gray')
     ax1.set_title(title1, fontsize=20)
     ax2.imshow(img2, cmap='gray')
     ax2.set_title(title2, fontsize=20)
--- threshold.py
+++ threshold.py
@@ -6,16 +6,14 @@ import myplot
 
 
 def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
-    # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     # Apply x or y gradient with the OpenCV Sobel() function
     # and take the absolute value
     if orient == 'x':
-        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
+        abs_sobel = np.absolute(cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
     if orient == 'y':
-        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
+        abs_sobel = np.absolute(cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
     # Rescale back to 8 bit integer
-    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
     # Create a copy and apply the threshold
     binary_output = np.zeros_like(scaled_sobel)
 
@@ -32,15 +30,13 @@ def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
 # Function to return the magnitude of the gradient for a given sobel kernel size
 # and threshold values
 def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
-    # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     # Take both Sobel x and y gradients
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
     # Calculate the gradient magnitude
     gradmag = np.sqrt(sobelx**2 + sobely**2)
     # Rescale to 8 bit
-    scale_factor = np.max(gradmag)/255
+    scale_factor = np.max(gradmag) / 255
     gradmag = (gradmag/scale_factor).astype(np.uint8)
     # Create a binary image of ones where threshold is met, zeros otherwise
     binary_output = np.zeros_like(gradmag)
@@ -54,12 +50,12 @@ def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
 
 # Applies Sobel x and y, then computes the direction of the gradient
 # and applies a threshold.
-def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
-    # Grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+def dir_threshold(img, sobel_kernel=3, thresh=(0, 180)):
+    # Convert threshold from degrees to radians
+    thresh = np.float32(thresh) * np.pi / 180
     # Calculate the x and y gradients
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
     # Take the absolute value of the gradient direction,
     # apply a threshold, and create a binary image result
     absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
@@ -74,17 +70,31 @@ def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
 
 def do_it(image):
     # Choose a Sobel kernel size
-    ksize = 5 # Choose a larger odd number to smooth gradient measurements
+    ksize = 11 # Choose a larger odd number to smooth gradient measurements
+
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    s_chan = hls[:, :, 2]
+
+    thresh = (15, 255)
+    s_bin = np.zeros_like(s_chan)
+    s_bin[(s_chan > thresh[0]) & (s_chan <= thresh[1])] = 1
+
+    #s_blur = cv2.blur(s_chan, (2, 2))
+    #myplot.plot_double(s_chan, s_bin, 'orig', 'binary')
+    img = s_bin
 
     # Apply each of the thresholding functions
-    gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(20, 255))
-    grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(20, 255))
-    mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(50, 255))
-    dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(.7, 1.3))
+    gradx = abs_sobel_thresh(img, orient='x', sobel_kernel=ksize, thresh=(200, 255))
+    #grady = abs_sobel_thresh(img, orient='y', sobel_kernel=ksize, thresh=(0, 255))
+    mag_binary = mag_thresh(img, sobel_kernel=ksize, mag_thresh=(0, 100))
+    dir_binary = dir_threshold(img, sobel_kernel=ksize, thresh=(0, 80))
 
     combined = np.zeros_like(dir_binary)
-    combined[((gradx == 1) & (grady == 1)) & ((mag_binary == 1) & (dir_binary == 1))] = 1
-    myplot.plot_double(image, combined, 'original image', 'thresholded image')
+    #combined[(gradx == 1) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+    #combined[((mag_binary == 1) & (dir_binary == 1))] = 1
+    combined = gradx
+
+    #myplot.timed_plot_double(image, combined, 'original image', 'thresholded image')
     return combined
 
 
